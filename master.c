688a36733332c8b63000ef0d68de73faa78204d8
Krish Agarwal <104710267+krivan0522@users.noreply.github.com>
2024-11-10 23:29:27 +0530

    commit x

--- master.c
+++ master.c
@@ -1,232 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <limits.h>
-#include <time.h>
-#include <signal.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/msg.h>
-#include <sys/sem.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
 
-#define probability 0.7
+#define p 0.05              // Probability of illegal page reference
 
-#define P(s) semop(s, &pop, 1)
-#define V(s) semop(s, &vop, 1)
+struct page_table {
+    int frame_number;
+    bool valid_bit;
+};
 
-typedef struct SM1
-{
-    int semid;              // semaphore id
-    int pid;                // process id
-    int pages_req;          // number of required pages
-    int frames_alloc;       // number of frames allocated
-    int pagetable[1000][3]; // page table
-    int page_faults;
-    int access_illegal;
-} SM1;
-
-int main()
-{
-    struct sembuf pop, vop;
-    pop.sem_num = 0;vop.sem_num = 0;
-    pop.sem_flg = 0;vop.sem_flg = 0;
-    pop.sem_op = -1;
-    vop.sem_op = 1;
-
-    srand(time(0));
+int main() {
+    srand(time(NULL));
 
     int k, m, f;
-    printf("Number of processes: ");
+    printf("Enter number of processes: ");
     scanf("%d", &k);
-    printf("Virtual Address Space size: ");
+    printf("Enter virtual space size (Max number of pages per process): ");
     scanf("%d", &m);
-    printf("Physical Address Space size: ");
+    printf("Enter number of frames: ");
     scanf("%d", &f);
 
-    char msgid1_str[100], msgid2_str[100], msgid3_str[100];
-    char shmid1_str[100], shmid2_str[100], shmid3_str[100];
-    char k_str[100];
+    // Create Data Structures
+    int shm1 = shmget(IPC_PRIVATE, k*m*(sizeof(struct page_table)), IPC_CREAT | 0666);      // Shared memory for page table. <frame_number, valid_bit>
+    int shm2 = shmget(IPC_PRIVATE, (f+1)*sizeof(int), IPC_CREAT | 0666);         // Shared memory for free frame list. First element is the number of free frames. Always pick the last frame.
+    int shm3 = shmget(IPC_PRIVATE, k*sizeof(int), IPC_CREAT | 0666);               // Shared memory for page number mapping
+    int mq1 = msgget(IPC_PRIVATE, IPC_CREAT | 0666);                         // Message queue for ready queue
+    int mq2 = msgget(IPC_PRIVATE, IPC_CREAT | 0666);                         // Message queue for scheduler-mmu communication
+    int mq3 = msgget(IPC_PRIVATE, IPC_CREAT | 0666);                         // Message queue for mmu-process communication
 
+    char mq1_str[10], mq2_str[10], mq3_str[10], shm1_str[10], shm2_str[10], shm3_str[10], k_str[10], m_str[10];
+    sprintf(mq1_str, "%d", mq1);
+    sprintf(mq2_str, "%d", mq2);
+    sprintf(mq3_str, "%d", mq3);
+    sprintf(shm1_str, "%d", shm1);
+    sprintf(shm2_str, "%d", shm2);
+    sprintf(shm3_str, "%d", shm3);
     sprintf(k_str, "%d", k);
+    sprintf(m_str, "%d", m);
 
-    // shared memory for per process page table
-    key_t key = ftok(".", 1);
-    int shmid1 = shmget(key, k * sizeof(SM1), IPC_CREAT | 0666);
-    SM1 *sm1 = (SM1 *)shmat(shmid1, NULL, 0);
-    sprintf(shmid1_str, "%d", shmid1);
-
-    // shared memory for list of free frames
-    key = ftok(".", 2);
-    int shmid2 = shmget(key, (f + 1) * sizeof(int), IPC_CREAT | 0666);
-    int *sm2 = (int *)shmat(shmid2, NULL, 0);
-    sprintf(shmid2_str, "%d", shmid2);
-
-    // shared memory for process to page mapping
-    key = ftok(".", 3);
-    int shmid3 = shmget(key, k * sizeof(int), IPC_CREAT | 0666);
-    int *sm3 = (int *)shmat(shmid3, NULL, 0);
-    sprintf(shmid3_str, "%d", shmid3);
-
-    // sm1 initialization
-    for (int i = 0; i < k; i++)
-    {
-        sm1[i].page_faults = 0;
-        sm1[i].access_illegal = 0;
-        key = ftok(".", i + 100); 
-        sm1[i].semid = semget(key, 1, IPC_CREAT | 0666);
-        semctl(sm1[i].semid, 0, SETVAL, 0);
+    // Initialize Data Structures
+    struct page_table * page_table = (struct page_table *) shmat(shm1, NULL, 0);
+    if (page_table == (void *) -1) {
+        perror("shmat");
+        // Handle the error...
+        return 1;
     }
 
-    // Free frames list initialization
-    // free -> 1, occupied -> 0, end of list -> -1
-    memset(sm2, 1, f * sizeof(int));
-    sm2[f] = -1;
-
-    // sm3 initialization
-    // initially no frames are allocated to any process
-    memset(sm3, 0, k * sizeof(int));
-
-    // semaphore for sched to signal master
-    key = ftok(".", 7);
-    int sem1 = semget(key, 1, IPC_CREAT | 0666);
-    semctl(sem1, 0, SETVAL, 0);
-
-    // Message Queue 1 for Ready Queue
-    key = ftok(".", 8);
-    int msgid1 = msgget(key, IPC_CREAT | 0666);
-    sprintf(msgid1_str, "%d", msgid1);
-
-    // Message Queue 2 for Scheduler
-    key = ftok(".", 9);
-    int msgid2 = msgget(key, IPC_CREAT | 0666);
-    sprintf(msgid2_str, "%d", msgid2);
-
-    // Message Queue 3 for Memory Management Unit
-    key = ftok(".", 10);
-    int msgid3 = msgget(key, IPC_CREAT | 0666);
-    sprintf(msgid3_str, "%d", msgid3);
-
-    int sched_pid, mmu_pid;
-
-    // create Scheduler process, pass msgid1_str, msgid2_str, k_str and shmid1_str
-    sched_pid = fork();
-    if (sched_pid == 0)
-    {
-        execlp("./sched", "./sched", msgid1_str, msgid2_str, k_str, shmid1_str, NULL);
+    for (int i = 0; i < k*m; i++) {
+        page_table[i].frame_number = -1;
+        page_table[i].valid_bit = false;
     }
-
-    // create Memory Management Unit process, pass msgid2_str, msgid3_str, shmid1_str and shmid2_str
-    mmu_pid = fork();
-    if (mmu_pid == 0)
-    {
-        execlp("xterm", "xterm", "-T", "Memory Management Unit", "-e", "./mmu", msgid2_str, msgid3_str, shmid1_str, shmid2_str, NULL);
+    int * free_frame_list = (int *) shmat(shm2, NULL, 0);
+    free_frame_list[0] = f;
+    for (int i = 1; i <= f; i++) {
+        free_frame_list[i] = i-1;
     }
+    int * page_number_mapping = (int *) shmat(shm3, NULL, 0);
 
-    // generate reference strings for each process
-    // int **reference = (int **)malloc(k * sizeof(int *));
-    // char **reference_str = (char **)malloc(k * sizeof(char *));
-    int reference[k][1000];
-    char reference_str[k][1000];
-
-    // initialize the processes
-    for (int i = 0; i < k; i++)
-    {
-        // generate random number of pages between 1 to m
-        sm1[i].pages_req = rand() % m + 1;
-        sm1[i].frames_alloc = 0;
-        for (int j = 0; j < m; j++)
-        {
-            sm1[i].pagetable[j][0] = -1;      // no frame allocated
-            sm1[i].pagetable[j][1] = 0;       // invalid
-            sm1[i].pagetable[j][2] = 1e9; // timestamp
-        }
-
-        int dig = 0;
-        int x = rand() % (8 * sm1[i].pages_req + 1) + 2 * sm1[i].pages_req;
-
-        // reference[i] = (int *)malloc(x * sizeof(int));
-
-        for (int j = 0; j < x; j++)
-        {
-            reference[i][j] = rand() % sm1[i].pages_req; // valid page number
-            int temp = reference[i][j];
-            do
-            {
-                temp /= 10;
-                dig++;
-            } while (temp > 0);
-            dig++;
-        }
-        dig++;
-
-        // with probability = probability, corrupt the reference string with illegal page number
-        for (int j = 0; j < x; j++)
-        {
-            double random_value = (double)rand() / (double)RAND_MAX; // Random value between 0 and 1
-            if (random_value < probability)
-            {
-                reference[i][j] = rand() % m; // Assign a random value from 0 to m-1
-            }
-        }
-
-        // reference_str[i] = (char *)malloc(dig * sizeof(char));
-        // memset(reference_str[i], '\0', dig);
-
-        for (int j = 0; j < x; j++)
-        {
-            char temp[100];
-            memset(temp, '\0', 100);
-            sprintf(temp, "%d.", reference[i][j]);
-            strcat(reference_str[i], temp);
+    // Pages per process and frame allocation
+    int total_pages = 0;
+    for (int i = 0; i < k; i++) {
+        page_number_mapping[i] = rand()%m + 1;
+        total_pages += page_number_mapping[i];
+    }
+    for (int i = 0; i < k; i++) {
+        int num_frames = (int)((float)page_number_mapping[i]/total_pages*f);
+        for (int j = 0; j < num_frames; j++) {
+            page_table[i*m+j].frame_number = free_frame_list[free_frame_list[0]];         
+            free_frame_list[0]--;
+            page_table[i*m+j].valid_bit = true;
         }
     }
 
-    // create Processes
-    for (int i = 0; i < k; i++)
-    {
-        sleep(1);
-        int pid = fork();
-        if (pid)
-        {
-            sm1[i].pid = pid;
-        }
-        else
-        {
-            execlp("./process", "./process", reference_str[i], msgid1_str, msgid3_str, k_str, shmid1_str, NULL);
-        }
+    printf("initialization done\n");
+    fflush(stdout);
+
+    
+    // Create scheduler
+    int scheduler_pid = fork();
+    if (scheduler_pid == 0) {
+        char *args[] = {"./scheduler", mq1_str, mq2_str, k_str, NULL};
+        execvp(args[0], args);
     }
 
-    // wait for Scheduler to signal
-    P(sem1);
+    // Create mmu in xterm
+    int mmu_pid = fork();
+    if (mmu_pid == 0) {
+        char *args[] = {"xterm", "-T", "MMU", "-e", "./mmu", mq2_str, mq3_str, shm1_str, shm2_str, shm3_str , m_str, k_str, NULL};
+        execvp(args[0], args);
+    }
 
-    printf("Master: All processes have completed. Quiting...\n");
+    // Create processes
+    for (int i = 0; i < k; i++) {
+        int x = 2*page_number_mapping[i] + rand()%(8*page_number_mapping[i] + 1);       // Length of reference string
+        int ref_string[x+1];
+        int num_digits = 0;
+        for (int j = 0; j < x; j++) {
+            if ((float)rand() / (float)RAND_MAX < p) ref_string[j] = page_number_mapping[i] + rand()%(f-page_number_mapping[i] + 1);        // Illegal page reference
+            else ref_string[j] = rand()%page_number_mapping[i];        // Legal page reference
+            num_digits += snprintf(NULL, 0, "%d", ref_string[j]);
+        }
+        ref_string[x] = -9;
+        num_digits += snprintf(NULL, 0, "%d", ref_string[x]);
+        char ref_string_str[num_digits+x+1];        // Print numbers with spaces
+        ref_string_str[0] = '\0';
+        for (int j = 0; j <= x; j++) {
+            char temp[10];
+            sprintf(temp, "%d", ref_string[j]);
+            strcat(ref_string_str, temp);
+            strcat(ref_string_str, " ");
+        }
 
-    // terminate Scheduler
-    kill(sched_pid, SIGINT);
+        int process_pid = fork();
+        if (process_pid == 0) {
+            char i_str[10];
+            sprintf(i_str, "%d", i);
+            printf("Process %d created with reference string %s\n", i, ref_string_str);
+            char *args[] = {"./process", mq1_str, mq3_str, ref_string_str, i_str, NULL};
+            execvp(args[0], args);
+        }
 
-    // terminate Memory Management Unit
-    kill(mmu_pid, SIGINT);
+        usleep(250000);        // Sleep for 250 ms
+    }
 
-    // detach and remove shared memory
-    shmdt(sm1);
-    shmctl(shmid1, IPC_RMID, NULL);
+    msgrcv(mq1, NULL, 0, 2, MSG_NOERROR);        // Wait for all processes to finish. Message type = 2
 
-    shmdt(sm2);
-    shmctl(shmid2, IPC_RMID, NULL);
+    usleep(1000);      // Giving time to mmu to cleanup
+    // Terminating scheduler and mmu
+    kill(scheduler_pid, SIGKILL);
+    kill(mmu_pid, SIGKILL);
 
-    shmdt(sm3);
-    shmctl(shmid3, IPC_RMID, NULL);
+    // Detach and remove shared memory
+    shmdt(page_table);
+    shmdt(free_frame_list);
+    shmdt(page_number_mapping);
+    shmctl(shm1, IPC_RMID, NULL);
+    shmctl(shm2, IPC_RMID, NULL);
+    shmctl(shm3, IPC_RMID, NULL);
 
-    // remove semaphore
-    semctl(sem1, 0, IPC_RMID, 0);
-
-    // remove message queues
-    msgctl(msgid1, IPC_RMID, NULL);
-    msgctl(msgid2, IPC_RMID, NULL);
-    msgctl(msgid3, IPC_RMID, NULL);
-
-    return 0;
+    // Remove message queues
+    msgctl(mq1, IPC_RMID, NULL);
+    msgctl(mq2, IPC_RMID, NULL);
+    msgctl(mq3, IPC_RMID, NULL);
 }
